ac5ee1803f2f8fc862d16224cef447cf2c6b183b
Carlos Maldonado <carlos.maldonado1@cloud.uautonoma.cl>
2022-01-02 14:03:03 -0500

    Agregar campos formulario y muestra en resultados

--- aplicaciones/persona/models.py
+++ aplicaciones/persona/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.utils.translation import deactivate
 
 class Persona(models.Model):
     id = models.AutoField(primary_key= True)
     nombre = models.CharField(max_length= 100)
-    apellido = models.CharField(max_length= 100)
-    correo = models.CharField(max_length= 100)
+    apellido = models.CharField(max_length= 100)    
+    correo = models.EmailField(max_length= 100)
+    genero = (('Masculino',"Masculino"),('Femenino',"Femenino"))
+    sexo = models.CharField(max_length= 20, choices= genero, default="Masculino")
+    edad = models.PositiveSmallIntegerField(default=1)
     
